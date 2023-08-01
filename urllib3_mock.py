038e4114313f87b0ae1272addd242f554081f62b
Eran Sandler <eran@sandler.co.il>
2023-07-31 19:13:24 -0700

    move to use the Signature object

--- urllib3_mock.py
+++ urllib3_mock.py
@@ -42,13 +42,11 @@ __all__ = ['Responses']
 def get_wrapped(func, wrapper_template, evaldict):
     # Preserve the argspec for the wrapped function so that testing
     # tools such as pytest can continue to use their fixture injection.
-    args, a, kw, defaults = inspect.signature(func)
+    sig = inspect.signature(func)
 
-    signature = inspect.formatargspec(args, a, kw, defaults)
     is_bound_method = hasattr(func, '__self__')
     if is_bound_method:
-        args = args[1:]     # Omit 'self'
-    callargs = inspect.formatargspec(args, a, kw, None)
+        sig.parameters = sig.parameters[1:]     # Omit 'self'
 
     ctx = {'signature': signature, 'funcargs': callargs}
     _exec(wrapper_template % ctx, evaldict)
